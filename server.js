d5439b6433faccf814115fab245aff28b4551f48
东风雨 <dfy3000@126.com>
2016-11-01 15:21:30 +0800

    调试代码.

--- server.js
+++ server.js
@@ -2,101 +2,126 @@ var express = require('express');
 var port = 8080;
 var app = express();
 
-var Course_List = [
-	{name:'English',id:0},
-	{name:'Math',id:1},
-	{name:'History',id:2},
-	{name:'IT',id:3}
-];
+var Course_List = [{
+    name: 'English',
+    id: 0
+}, {
+    name: 'Math',
+    id: 1
+}, {
+    name: 'History',
+    id: 2
+}, {
+    name: 'IT',
+    id: 3
+}];
 var course_count = Course_List.length;
 
-var Student_List = [
-	{id:1,name:'Student A',course:[]},
-	{id:2,name:'Student B',course:[]},
-	{id:3,name:'Student C',course:[]},
-	{id:4,name:'Student D',course:[]}
-];
+var Student_List = [{
+    id: 1,
+    name: 'Student A',
+    course: []
+}, {
+    id: 2,
+    name: 'Student B',
+    course: []
+}, {
+    id: 3,
+    name: 'Student C',
+    course: []
+}, {
+    id: 4,
+    name: 'Student D',
+    course: []
+}];
 
 app.use(express.static('web'));
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
     res.sendfile('web/index.html');
 });
 
-app.get('/courses',function(req, res){
-	res.send(Course_List);
+app.get('/courses', function(req, res) {
+    res.send(Course_List);
 });
 
-app.get('/course/add',function (req, res) {
-	var _name = req.param('name');
-	var _i = Course_List.find(function(course){
-		return course.name === _name;
-	});
+app.get('/course/add', function(req, res) {
+    var _name = req.param('name');
+    var _i = Course_List.findIndex(function(course) {
+        console.log(course);
+        return course.name === _name;
+    });
+    console.log('add index ', _i);
 
-	if(_i > -1){
-		res.send('error');
-	}else{
-		course_count++;
-		Course_List.push({name:_name,id:course_count});
-		res.send('success');
-	}
+    if (_i > -1) {
+        res.send('Error or Already exist ');
+    } else {
+        course_count++;
+        Course_List.push({
+            name: _name,
+            id: course_count
+        });
+        res.send('success');
+    }
 });
 
-app.get('/course/delete', function(req, res) {
-	var _id = req.param('id');
-	var _i = Course_List.find(function(course){
-		return course.id === _id;
-	});
-	if(_i > -1){
-		Course_List.splice(_i, 1);
-		res.send('success');
-	}else{
-		res.send('error')
-	}
+app.get('/course/delete/:id', function(req, res) {
+    var _id = req.params.id;
+    var _i = Course_List.findIndex(function(course) {
+        return course.id == _id;
+    });
+		console.info('_i  is ', _i);
+    if (_i > -1) {
+        Course_List.splice(_i, 1);
+        res.send('success');
+    } else {
+        res.send('error or don\'t find');
+    }
 });
 
-app.get('/course/edit', function (req, res) {
-	var  _id = req.param('id');
-	var _name = req.param('name');
-	var _i = Course_List.find(function(course){
-		return course.id === _id;
-	});
-	if(_i > -1){
-		Course_List.splice(_i, 1);
-		res.send('success');
-	}else{
-		res.send('error')
-	}
-});
-app.get('/course/search', function (req, res) {
-	var  _s = req.param('s').toLocaleLowerCase();
-	var _arr = [];
-	for(var i=0; i < Course_List.length; i++){
-		if(Course_List[i].name.toLocaleLowerCase().includes(_s)){
-			_arr.push(Course_List[i]);
-		}
-	}
-	res.send(_arr)
-});
-
-app.get('/studnets',function(req, res){
-	res.send(Student_List);
-});
-
-app.get('/student/enroll_course', function (req, res) {
+app.get('/course/edit', function(req, res) {
     var _id = req.param('id');
-    var _courses = req.param('course_list');
-	var _i = Course_List.find(function(course){
-		return course.id === _id;
-	});
-	if(_i > -1){
-		Student_List[_i].course = _courses;
-		res.send('success')
-	}else{
-		res.send('error')
-	}
+    var _name = req.param('name');
+    var _i = Course_List.findIndex(function(course) {
+        return course.id == _id;
+    });
+    if (_i > -1) {
+        Course_List[_i].name = _name;
+        res.send('success');
+    } else {
+        res.send('error')
+    }
 });
 
-app.listen(port, function () {
+app.get('/course/search', function(req, res) {
+    var _s = req.param('s').toLocaleLowerCase();
+    var _arr = [];
+    for (var i = 0; i < Course_List.length; i++) {
+        if (Course_List[i].name.toLocaleLowerCase().includes(_s)) {
+            _arr.push(Course_List[i]);
+        }
+    }
+    res.send(_arr);
+});
+
+app.get('/studnets', function(req, res) {
+    res.send(Student_List);
+});
+
+app.get('/student/enroll_course', function(req, res) {
+    var _id = req.param('id');
+    var _courses = req.param('courses');
+    var _i = Course_List.findIndex(function(course) {
+        return course.id == _id;
+    });
+    if (_i > -1) {
+        Student_List[_i].course = _courses;
+        res.send('success')
+    } else {
+        res.send('error')
+    }
+});
+
+app.listen(port, function() {
     console.log('app is listening at port ', port);
 });
--- web/app.js
+++ web/app.js
@@ -1,23 +1,51 @@
 // var app = angular.module('courseWeb', []);
 
-$(function(){
-	// $.get('/courses',function(res){
-	// 	console.log(res)
-	// });
+$(function() {
+    $.get('/courses', function(res) {
+        console.log(res)
+    });
 
-	$.get('/course/add',{name:'haha'},function(res){
-		$.get('/courses',function(res){
-			console.log('courses ： ',res)
-		});
-	});
+    // $.get('/course/add',{name:'haha'},function(res){
+    // 	$.get('/courses',function(res){
+    // 		console.log('courses ： ',res)
+    // 	});
+    // });
+    //
+    // $.get('/course/search',{'s':'a'},function(res){
+    // 	console.log('Search Result ：',res)
+    // });
+    //
+    // //
+    // $.get('/studnets',function(res){
+    // 	console.log('获取所有学生 ',res)
+    // });
 
-	$.get('/course/search',{s:'e'},function(res){
-		console.log('Search Result ：',res)
-	});
+    //
+    // $.get('/course/delete/0', function(res) {
+    //     console.log('删除课程 ', res);
+    //     $.get('/courses', function(res) {
+    //         console.log(res)
+    //     });
+    // });
 
-	$.get('/studnets',function(res){
-		console.log(res)
-	});
+    // $.get('/course/edit',{id:1,name:Date()},function(res){
+    // 	console.log(' 编辑结果 ', res);
+    // 	$.get('/courses', function(res) {
+    //       console.log(res)
+    //   });
+    // });
 
+    $.get('/studnets', function(res) {
+        console.log(' studnets is ', res);
+    });
 
+    $.get('/student/enroll_course', {
+        id: 1,
+        courses: [1, 2]
+    }, function(res) {
+        console.log(' enroll course is ', res);
+        $.get('/studnets', function(res) {
+            console.log(' studnets is ', res);
+        });
+    });
 });
