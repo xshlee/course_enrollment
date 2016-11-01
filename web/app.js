bdc5e3d73745c0dccc3b1324d73b9154529255c7
东风雨 <dfy3000@126.com>
2016-11-01 15:24:08 +0800

    放开注释.

--- web/app.js
+++ web/app.js
@@ -5,35 +5,35 @@ $(function() {
         console.log(res)
     });
 
-    // $.get('/course/add',{name:'haha'},function(res){
-    // 	$.get('/courses',function(res){
-    // 		console.log('courses ： ',res)
-    // 	});
-    // });
-    //
-    // $.get('/course/search',{'s':'a'},function(res){
-    // 	console.log('Search Result ：',res)
-    // });
-    //
-    // //
-    // $.get('/studnets',function(res){
-    // 	console.log('获取所有学生 ',res)
-    // });
+    $.get('/course/add',{name:'haha'},function(res){
+    	$.get('/courses',function(res){
+    		console.log('courses ： ',res)
+    	});
+    });
+
+    $.get('/course/search',{'s':'a'},function(res){
+    	console.log('Search Result ：',res)
+    });
 
     //
-    // $.get('/course/delete/0', function(res) {
-    //     console.log('删除课程 ', res);
-    //     $.get('/courses', function(res) {
-    //         console.log(res)
-    //     });
-    // });
+    $.get('/studnets',function(res){
+    	console.log('获取所有学生 ',res)
+    });
 
-    // $.get('/course/edit',{id:1,name:Date()},function(res){
-    // 	console.log(' 编辑结果 ', res);
-    // 	$.get('/courses', function(res) {
-    //       console.log(res)
-    //   });
-    // });
+
+    $.get('/course/delete/0', function(res) {
+        console.log('删除课程 ', res);
+        $.get('/courses', function(res) {
+            console.log(res)
+        });
+    });
+
+    $.get('/course/edit',{id:1,name:Date()},function(res){
+    	console.log(' 编辑结果 ', res);
+    	$.get('/courses', function(res) {
+          console.log(res)
+      });
+    });
 
     $.get('/studnets', function(res) {
         console.log(' studnets is ', res);
